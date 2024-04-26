d5633092afc763feaa2d4a6462a56107e5124ec1
teamqaqacraft <gaurang@qacraft.com>
2024-04-26 17:42:01 +0530

    26-04-24_slp_option
    create new class slp_option
    verify two buttons read more and view available hours
    update xpath for onboarding page

---
+++ Better_Speech_Test_Automation/tests/slp_option_Test.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect, Page } from '@playwright/test'
+import slpoptionpage from '../../Better_Speech_Test_Automation/pages/slp_option_page';
+import { allure } from 'allure-playwright';
+
+
+
+test.describe.serial('slpoption_page', () => {
+
+    let slpoption_page: slpoptionpage;
+
+    test.beforeEach(async ({ page }) => {
+        slpoption_page = new slpoptionpage(page);
+        await slpoption_page.navigate();
+
+    });
+    test('Testcase_001_Check Two Buttons on SLP option', async ({ page, context }) => {
+        await allure.step("Step_001_Verify Read more and View Available Hours are works", async () => {
+        });
+        await slpoption_page.Login_With_cread();
+    });
+});
