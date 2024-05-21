cfd2dc4f25867f9ce01b246874a691993fc34aa2
AkshayKankodiya <akshay@qacraft.com>
2024-05-21 19:18:30 +0530

    slp_public Test

---
+++ Better_Speech_Test_Automation/tests/slp_public_Test.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect, Page } from '@playwright/test'
+import slpoptionpage from '../pages/slp_public_page';
+import { allure } from 'allure-playwright';
+import slppublic from '../pages/slp_public_page';
+
+
+
+test.describe.serial('slp_public_page', () => {
+
+    let slppublc_page: slppublic;
+
+    test.beforeEach(async ({ page }) => {
+        slppublc_page = new slppublic(page);
+        await slppublc_page.navigate();
+
+    });
+    test('Testcase_001_Verify SLP Public ', async ({ page, context }) => {
+        await allure.step("Step_001_Verify SLP Public", async () => {
+        });
+        await slppublc_page.verify_elem_without_Login();
+        await slppublc_page.verify_elem_with_Login()
+    });
+});
